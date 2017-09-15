d97deff3dcedd9c3b25bffe009fb7888eebd330d
kdurril <kdurril@gmail.com>
2017-09-15 14:54:33 -0400

    remove unused code

--- parse.py
+++ parse.py
@@ -114,25 +114,6 @@ class tableParse(object):
 
     def full_record(self):
         return (self.summary,self.client(),list(self.compliance_dict()))
- 
-def get_table(soup=None):
-        "treive html tables"
-        for table in soup.body.find_all('table'):
-            if not table.table:            
-                yield table
-
-def parse_table(table=None):
-        for element in table:
-            if element != '\n':
-                yield element
-
-def parse_row(table_element=None):
-        for row in table_element:
-            yield tuple(cell.string for cell in row.find_all('td'))
-
-def test_parse(soup=None):
-        for table in get_table(soup):
-            yield parse_row(table_element=parse_table(table=table))
 
 def test_gather():
     coll = []
@@ -144,22 +125,3 @@ def test_gather():
             pars = tableParse(soup,record_id)
             coll.append(pars)
     return coll
-
-
-
-#b = test_gather()
-#c = b[0].alt_parse()
-#d = [list(i) for i in c]
-#f = [i for i in d[2][0].children]
-#[[x.string for x in y.children if x.string != None] for y in [list(i) for i in b[2].alt_parse()][2][0].children if y != '\n']
-#[[x.string for x in y.children if x.string != None] for y in [list(i) for i in self.alt_parse()][2][0].children if y != '\n']
-
-def client():
-    count = 0
-    for i in self.alt_parse():
-        yield list(i)
-        count += 1
-
-    for y in [list(i) for i in self.alt_parse()][2][0].children: 
-        if y != '\n':
-            yield y
